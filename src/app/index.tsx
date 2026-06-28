20861d976c3b01c1ec80a0c1ce19eaf8aadb626f
zakilisee <zakilisee@gmail.com>
2026-06-28 23:42:50 +0100

    chore: update dependencies and configure Nativewind
    - Added Nativewind as a dependency in package.json.
    - Updated react-native-reanimated version to use caret notation.
    - Imported global CSS in _layout.tsx for styling.
    - Refactored index.tsx to use Nativewind classes instead of StyleSheet.
    - Created tailwind.config.js for Nativewind configuration.
    - Added nativewind-env.d.ts to tsconfig.json for TypeScript support.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,16 +2,8 @@ import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 items-center justify-center">
       <Text>Hello World!</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});

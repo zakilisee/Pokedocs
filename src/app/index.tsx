ebca9ed13ef06647ef8062a7a43fa3824fde52ea
zakilisee <zakilisee@gmail.com>
2026-06-29 20:39:20 +0100

    feat: fetch and display Pokémon data in the Index component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,27 @@
-import { Text, View, StyleSheet } from "react-native";
+import { useEffect } from "react";
+import { Text, View } from "react-native";
+
 
 export default function Index() {
+  let data;
+  useEffect(()=>{
+    fetchPokemon()
+  },[])
+
+  async function fetchPokemon() {
+    try{
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/")
+    data = await response.json();
+    console.log(data);
+    }catch(e){
+      console.log(e);
+    }
+
+  }
   return (
-    <View className="flex-1 items-center justify-center">
+    <View>
       <Text>Hello World!</Text>
+      <Text> this is the data you wanted:{data}</Text>
     </View>
   );
 }

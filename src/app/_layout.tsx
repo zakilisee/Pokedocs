f2349e5379b93ca919c0541d099a47c2fc6902a6
zakilisee <zakilisee@gmail.com>
2026-06-30 17:41:30 +0100

    feat: implement RootLayout with Stack and Screen for navigation

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
 import "../../global.css"
 export default function RootLayout() {
-  return <Stack />;
+  return (
+  <Stack >
+    <Stack.Screen name="index"  options={{
+      title : "PokeDocs",
+      headerStyle : {backgroundColor : 'red'},
+      }}/>
+  </Stack>);
 }

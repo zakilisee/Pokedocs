d5d77bcf986c33b7de74b0c14b6d3455f002ace2
zakilisee <zakilisee@gmail.com>
2026-07-02 00:24:11 +0100

    feat: fetch and display base experience in DetailsScreen

--- src/app/DetailsScreen.tsx
+++ src/app/DetailsScreen.tsx
@@ -1,13 +1,41 @@
 import { useLocalSearchParams } from 'expo-router'
+import { useEffect, useState } from 'react';
 import { View, Text } from 'react-native'
 
+
+interface detailsState{
+  base_experience : number,
+}
 const DetailsScreen = () => {
-  const params = useLocalSearchParams();
+  const params = useLocalSearchParams() as {
+  url: string;
+  };
+
+  const url = params.url;
+
+  useEffect(() =>{
+    fetchPokemonDetails();
+  },[])
+
+  const [details, setDetails] = useState<detailsState>({
+    base_experience : 0,
+  })
+
+  async function fetchPokemonDetails(){
+    try{
+      const result = await fetch(url);
+      const data = await result.json();
+      setDetails(data);
+
+    }catch(e){
+      console.log(e)
+    }
+  }
 
   console.log(params);
   return (
     <View>
-      <Text>details</Text>
+      <Text>{details.base_experience}</Text>
     </View>
   )
 }
